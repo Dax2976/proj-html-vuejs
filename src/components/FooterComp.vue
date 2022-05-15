bceb9352c0bd9abcb584546fe158e436924b56e3
Dax2976 <ferrodavide55@gmail.com>
2022-05-16 00:19:48 +0200

    fix footer need to rework

--- src/components/FooterComp.vue
+++ src/components/FooterComp.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="container-fluid yellow d-flex">
-        <div class="row"></div>
+        <div class="row">
+            <div class="about">
+                <a href="#" class="text-uppercase">About</a>
+            </div>
+            <div class="contacts">
+                <a href="#" class="text-uppercase">contacts</a>
+            </div>
+            <div class="pages">
+                <a href="#" class="text-uppercase">pages</a>
+            </div>
+            <div class="blog">
+                <a href="#" class="text-uppercase">blog</a>
+            </div>
+        </div>
     </div>
     
 </template>
@@ -14,5 +27,9 @@ export default {
 <style lang="scss" scoped>
 .yellow{
     background-color: #f2b71b;
+}
+a{
+    text-decoration: none;
+    color: black;
 }
 </style>
--- src/components/MyCourse.vue
+++ src/components/MyCourse.vue
@@ -2,46 +2,40 @@
     <div class="container d-flex">
         <div class="row">
             <h1 class="text-center my-5"><strong>Why My course?</strong></h1>
-            <div class="col-6">
+            <div class="col-5">
                 <div class="col align-items-start">
                     <div class="icon">
                         <font-awesome-icon icon="fa-solid fa-briefcase" />
                     </div>
                 </div>
-                <div class="col-5 left align-items-end">
+                <div class="col-4 left align-items-end">
                     <h3 class="text-center"><strong>Entrepreneurship</strong></h3>
                     <p class="textLow">Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate</p>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="col align-items-start">
+            <div class="col-5">
                     <div class="icon">
                         <font-awesome-icon icon="fa-solid fa-rocket"  />
                     </div>
-                </div>
-                <div class="col-5 left align-items-end">
+                <div class="col-4 left align-items-end">
                     <h3 class="text-center"><strong>Accelerated learning</strong></h3>
                     <p class="textLow">Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate</p>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="col align-items-start">
+            <div class="col-5">
                     <div class="icon">
                         <font-awesome-icon icon="fa-regular fa-thumbs-up"  />
                     </div>
-                </div>
-                <div class="col-5 left align-items-end">
+                <div class="col-4 left align-items-end">
                     <h3 class="text-center"><strong>Productivity</strong></h3>
                     <p class="textLow">Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate</p>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="col align-items-start">
-                    <div class="icon">
-                        <font-awesome-icon icon="fa-solid fa-gear" />
-                    </div>
+            <div class="col-5">
+                <div class="icon">
+                    <font-awesome-icon icon="fa-solid fa-gear" />
                 </div>
-                <div class="col-5 left align-items-end">
+                <div class="col-4 left align-items-end">
                     <h3 class="text-center"><strong>Life Coaching</strong></h3>
                     <p class="textLow">Vestibulum vitae aliquam nunc. Suspendisse mollis metus ac tellus egestas pharetra. Suspendisse at viverra purus. Pellentesque nec posuere ligula, eu congue leo. Integer vulputate tempor arcu. Vestibulum vulputate</p>
                 </div>
@@ -76,20 +70,21 @@ export default {
         
     }
 
-    .col-5{
+    .col-4{
         left: 250px;
         position: relative;
         bottom: 120px;
     }
 
-    .col-6{
+    .col-5{
         height: 35%;
         border-left: 6px solid #f2b71d;
         box-shadow: 0 6px 20px 0 rgb(0 0 0 / 14%);;
+        margin: 40px;
         
     }
 
-    .col-6:hover{
+    .col-5:hover{
         background-color: #f2b71d;
     }
 
